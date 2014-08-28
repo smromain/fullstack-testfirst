4e7972c43c6eccc7c1d01713720184934e4a2e41
Steve Romain <smromain@gmail.com>
2014-08-28 10:27:40 -0400

    Cleaned up Calculator.JS code.

--- 03_calculator/calculator.js
+++ 03_calculator/calculator.js
@@ -1,29 +1,16 @@
 function Calculator() {
-	// {}
 	this.baseline = 0;
-	
-	// return {basline: 0}
-	// return this.baseline;
 };
 
 // {basline: 0}
 Calculator.prototype.value = function(){
 	return this.baseline;
-	
-	// this.value = this.baseline;
-	// {
-	// 	basline:0, 
-	// 	value:0
-	// }
-	// return this.value;
 };
 
 Calculator.prototype.add = function(x) {
 	this.baseline += x;
-	// return this.value;
 };
 
 Calculator.prototype.subtract = function(x) {
 	this.baseline -= x;
-	// return this.value;
 };
